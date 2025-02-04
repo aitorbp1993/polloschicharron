6f22f21e3cd7a6e8a0960469518a203f19522e5d
aitorbp <aitor.tsf@gmail.com>
2025-02-04 13:07:37 +0100

    auditoriess

---
+++ src/main/java/com/sinensia/polloschicharron/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.sinensia.polloschicharron.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.sinensia.polloschicharron.business.services.impl.AuditInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuditInterceptor auditInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(auditInterceptor);
+    }
+}
